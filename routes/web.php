455b439cb6f826bf357eab1c475c1f36b5af120e
Steve FASSEU <sfasseu@gmail.com>
2026-02-20 15:51:38 +0100

    Revert "premier commit - structure propre"
    This reverts commit 088cad6ecbf26ea5552c56ae3203166d7fa73af8.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,10 +16,3 @@ use App\Http\Controllers\ContactController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/home',[HomeController::class , 'index']);
-Route::get('/contact',[ContactController::class , 'index']);
-Route::get('/create',[ContactController::class ,'create'])->name('contact.create');
-Route::post('/store',[ContactController::class, 'store' ])->name('contact.store');
-Route::delete('/delete',[ContactController::class, 'destroy'])->name('contact.destroy');
-Route::put('/edit', [ContactController::class, 'edit'])->name('contact.edit');
