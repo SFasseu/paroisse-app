08b0729c29084b2cfd00a02ca495c2da9bf5fede
jordanTsane <jordantsane237@gmail.com>
2026-02-23 14:13:21 +0100

    init branch jordan

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<htm lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -58,4 +58,4 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+</htm
